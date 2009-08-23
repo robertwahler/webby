e4ef272cc8fc55ca0aec90d3b1949b6d07f0f298
Aggelos Orfanakos <agorf@agorf.gr>
2009-08-23 12:43:23 +0300

    Add support for options in Markdown filter

--- lib/webby/filters/markdown.rb
+++ lib/webby/filters/markdown.rb
@@ -2,8 +2,17 @@
 # Render text via markdown using the RDiscount library.
 if try_require('rdiscount', 'rdiscount')
 
+  Loquacious.configuration_for(:webby) {
+    desc <<-__
+      An array of options that will be passed to RDiscount when procesing
+      content. See the RDiscount rdoc documentation for the list of available
+      options.
+    __
+    markdown_options %w(smart)
+  }
+
   Webby::Filters.register :markdown do |input|
-    RDiscount.new(input).to_html
+    RDiscount.new(input, *Webby.site.markdown_options).to_html
   end
 
 # Otherwise raise an error if the user tries to use markdown
